758509d186be8ac3346bd01266320b8296f180da
Anderson <yrgenius80@gmail.com>
2021-09-19 21:52:27 +0300

    html change header

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ function styles(){
     .pipe(scss({outputStyle: 'compressed'}))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-    .pipe(dest('app/css'))
+    .pipe(dest('app/css/'))
     .pipe(browserSync.stream()) 
 }
 
