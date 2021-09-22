6ea7e1ee700bc32576099cb940f7db66ee8cfffe
Anderson <yrgenius80@gmail.com>
2021-09-23 02:36:36 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,19 @@
 $(function() {
 
-    $('.slider__wrapper').slick({
+    let screenWidth = $(window).width();
 
+    $('.slider__wrapper').slick({
+        dots: false,
+        arrows: true
     });
+
+    if (screenWidth - 240 > 1370) {
+        $('.slick-prev').css({ 'top': '50%' });
+        $('.slick-next').css({ 'top': '50%' });
+    } else {
+        $('.slick-prev').css({ 'top': '85%' });
+        $('.slick-next').css({ 'top': '85%' });
+    }
+
 
 });
