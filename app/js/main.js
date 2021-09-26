ebf9d2bea8cd41f3752828bc4c20809bf30c70aa
Anderson <yrgenius80@gmail.com>
2021-09-26 16:33:31 +0300

    added select in header

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,32 @@
 $(function() {
 
     let screenWidth = $(window).width();
+    const select = $('.form__catalog');
+    let flagSelect = 0;
+
+    select.on('click blur', function(event) {
+        flagSelect = flagSelect + 1;
+        if (event.type == 'click' && flagSelect == 1) {
+            $(this).css({
+                'background-image': "url('../images/header/nav_icon.svg'), url('../images/header/select-arrow-up.svg')"
+            });
+        }
+
+        if (event.type == 'blur' || flagSelect != 1) {
+            $(this).css({
+                'background-image': "url('../images/header/nav_icon.svg'), url('../images/header/select-arrow-down.svg')"
+            });
+            flagSelect = 0;
+        }
+
+    });
+
 
     $('.slider__wrapper').slick({
         dots: false,
         arrows: true
     });
+
 
     if (screenWidth - 240 > 1370) {
         $('.slick-prev').css({ 'top': '50%' });
@@ -14,5 +35,8 @@ $(function() {
         $('.slick-prev').css({ 'top': '85%' });
         $('.slick-next').css({ 'top': '85%' });
     }
+
+
+
 
 });
